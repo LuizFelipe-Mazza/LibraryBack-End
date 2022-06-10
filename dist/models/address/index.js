6bf09b5b717ae9e3ebe5e7a9eab50e4d58e614df
felipe <luiz.fernandes@mazzafc.tech>
2022-06-10 12:39:02 -0300

    fix:remove Dist

--- dist/models/address/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.AddressRepository = void 0;
-const tslib_1 = require("tslib");
-const repository_1 = tslib_1.__importDefault(require("./repository"));
-exports.AddressRepository = repository_1.default;
-//# sourceMappingURL=index.js.map
