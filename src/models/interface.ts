355c504bcec8d4ac51fbad8149ea3a33e5c5c986
felipe <luiz.fernandes@mazzafc.tech>
2022-06-14 16:20:10 -0300

    doc:update interface and create news params

--- src/models/interface.ts
+++ src/models/interface.ts
@@ -2,14 +2,18 @@ export interface IRepository<T, D> {
   getById: (id: number) => Promise<T | undefined>
   update: (id: number, data: Partial<D>) => Promise<T>
   remove: (id: number) => Promise<void>
-  paginate: (params: Pagination<Partial<T>>) => Promise<PaginateReturnType<T>>
+  paginate: (params: Pagination) => Promise<T[]>
   create: (data: D) => Promise<T | undefined>
 }
 
-export type Pagination<Filter> = {
+export type Filter = Partial<{
+  [key: string]: string
+}>
+
+export type Pagination = {
   pageSize: number
-  page?: number
-  filter?: Filter | string | Object
+  page: number
+  filter: Filter
 }
 
 export type PaginateReturnType<T> = {
