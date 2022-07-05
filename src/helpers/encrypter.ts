cdc77bd8288dbd94b9b557c2865a1df911bb8e9f
felipe <felipekurosaki123@gmail.com>
2022-07-05 08:49:19 -0300

    wip: work in progress

--- src/helpers/encrypter.ts
+++ src/helpers/encrypter.ts
@@ -1,4 +1,18 @@
+import bcrypt from 'bcrypt'
 export interface IEncrypter {
-  encrypt(value: string): string
-  compare(valueToCompare: string, originalEncripted: string): boolean
+  encrypt(value: string): Promise<string>
+  compare(valueToCompare: string, originalEncripted: string): Promise<boolean>
+}
+export class Encrypter implements IEncrypter {
+  async encrypt(value: string): Promise<string> {
+    const saltRounds = 10
+    const salt = await bcrypt.genSalt(saltRounds)
+    return await bcrypt.hash(value, salt)
+  }
+  async compare(
+    valueToCompare: string,
+    originalEncripted: string,
+  ): Promise<boolean> {
+    return await bcrypt.compare(valueToCompare, originalEncripted)
+  }
 }
