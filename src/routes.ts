1e6354ef7a42c0d3fa3e80aeaf688821a68b0ca9
felipe <felipekurosaki123@gmail.com>
2022-07-05 08:52:02 -0300

    doc: completed routes, add new routes wich user, provider

--- src/routes.ts
+++ src/routes.ts
@@ -1,15 +1,25 @@
 import { Router } from "express";
 import addressController from './controller/addressController';
 import providerController from './controller/providerController'
+import userController from './controller/userController'
 
 const router = Router();
 router.get('/address/:id', addressController.address);
-router.post('/createaddress', addressController.createAddress);
 router.put('/update/:id', addressController.UpdateAddress);
+router.post('/createaddress', addressController.createAddress);
 router.delete('/remove/:id', addressController.deleteAddress);
 
 // Provider Area
 router.post('/createprovider', providerController.createProvider);
+router.put('/updateprovider', providerController.UpdateProvider)
+router.delete('/removeprovider/:id', providerController.deleteProvider)
 router.get('/showprovider/:id', providerController.provider);
 router.get('/provider/paginate', providerController.paginate);
+
+// User Area
+router.get('/user/:id', userController.user);
+router.put('/updateuser', userController.UpdateUser);
+router.post('/createuser', userController.createuser);
+router.delete('/deleteuser', userController.deleteUser);
+
 export default router;
