ef49f0ea817a9bc1d7854df2d3e1d0bc179dee28
felipe <felipekurosaki123@gmail.com>
2022-07-05 08:54:17 -0300

    doc: update table, and complet her

--- src/migrations/003_2022_06_29_add_users_table.ts
+++ src/migrations/003_2022_06_29_add_users_table.ts
@@ -4,14 +4,13 @@ export async function up(knex: Knex): Promise<void> {
   await knex.schema.createTable('user', function (table) {
     table.increments('id').primary()
     table.integer('id_persona').unsigned()
-    table.string('name').notNullable()
-    table.string('email').notNullable()
+    table.string('name',50).notNullable()
+    table.string('email',50).unique().notNullable()
+    table.string('password',255).notNullable()
     table.boolean('active').defaultTo(0)
     table.string('avatar').nullable()
     table.string('hash_recovery_password', 16).notNullable()
-    table
-      .datetime('date_agree_use_terms', { precision: 2 })
-      .defaultTo(knex.fn.now(6))
+    table.datetime('date_agree_use_terms', { precision: 2 }).defaultTo(knex.fn.now(2))
     table.string('token_google').nullable()
   })
 }
